52207eb29cb1cf70635da2c66c20764cb0ccda34
silviaemanueli <silvia.stadler@escola.pr.gov.br>
2025-05-13 14:28:17 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programação'
+    'O que é HtML?'
+    'Linguagem de marcação'
+)
+
+criaCartao(
+    'Inglês'
+    'Como é dia do pagamento?'
+    'Pay Day'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
