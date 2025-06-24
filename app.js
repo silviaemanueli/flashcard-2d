1b7e7172416e2c782bcb6e404cf8c830205f995f
silviaemanueli <silvia.stadler@escola.pr.gov.br>
2025-06-24 14:46:49 +0000

    ok

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function criaCartao(categoria, pergunta, resposta) {
                       <p>${pergunta}</p>
                     </div>
                     <div class="cartao__conteudo__resposta">
-                      <p>${resposta}Linguagem de programação </p>
+                      <p>${resposta}</p>
                     </div>
                     </div>
                     
